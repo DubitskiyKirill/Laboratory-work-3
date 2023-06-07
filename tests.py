44a81c80670cf9808d1894acae02a83d7b8699bc
DubitskiyKirill <125906191+DubitskiyKirill@users.noreply.github.com>
2023-06-07 09:05:38 +0600

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,30 @@
+import unittest
+from main import app
+class TestStringMethods(unittest.TestCase):
+    def test_page(self):  # тестируем функцию, которая отвечает за главную страницу
+        tester = app.test_client(self)  # создаем клиент для тестирования
+        response = tester.get('/')  # отправляем GET-запрос на главную страницу
+        self.assertEqual(response.status_code, 200)  # проверяем, что код ответа равен 200
+
+    def test_konvert_type_1_1(self):#Проверка 
+        tester = app.test_client(self)  # создаем клиент для тестирования
+        response = tester.post("/", content_type='multipart/form-data', data={'num_1': 1000, 'options': 1})#Ввод параметров
+        self.assertIn('12.490632025980513', response.data.decode())
+
+    def test_konvert_type_1_2(self):  # Второй тест работоспособности первого режима
+        tester = app.test_client(self)  # создаем клиент для тестирования
+        response = tester.post("/", content_type='multipart/form-data', data={'num_1': 1000,'options': 2})  # Ввод параметров
+        self.assertIn('11.641443538998836', response.data.decode())
+
+    def test_konvert_type_2_1(self):  # Первый тест работоспособности второго режима
+        tester = app.test_client(self)  # создаем клиент для тестирования
+        response = tester.post("/", content_type='multipart/form-data', data={'num_1': 100, 'options': 3})  # Ввод параметров
+        self.assertIn('8006.', response.data.decode())
+
+    def test_konvert_type_2_2(self):  # Второй тест работоспособности второго режима
+        tester = app.test_client(self)  # создаем клиент для тестирования
+        response = tester.post("/", content_type='multipart/form-data', data={'num_1': 100, 'options': 4})  # Ввод параметров
+        self.assertIn('8590.', response.data.decode())
+
+if __name__ == '__test__':
+    unittest.test()

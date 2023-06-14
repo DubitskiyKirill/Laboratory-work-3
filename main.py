8f87c80c011bb68824d134e6cf51583909142cce
DubitskiyKirill <125906191+DubitskiyKirill@users.noreply.github.com>
2023-06-15 01:02:19 +0600

    Update main.py

--- main.py
+++ main.py
@@ -14,19 +14,18 @@ def form():
         sum = int(request.form.get('num_1'))
         type = request.form['options']
     if type == '1':#Доллар
-        answer=sum*80.06
-        return render_template('index.html', ans= str(str(sum)+" Рублей - " + str(answer) +" Доллар"))
+        answer=(sum/80.06)
+        return render_template('index.html', ans= str(str(sum)+" Рублей - " + str(round(answer,2)) +" Доллар"))
     if type == '2':#Евро
-        answer = sum * 85.9
-        return render_template('index.html', ans=str(str(sum) + " Рублей - " + str(answer) + " Евро"))
-    if type == '3':  # Доллар
-        answer = sum / 80.06
-        return render_template('index.html', ans=str(str(sum) + " Доллар - " + str(answer) + " Рублей"))
-    if type == '4':  # Евро
         answer = sum / 85.9
-        return render_template('index.html', ans=str(str(sum) + " Евро - " + str(answer) + " Рублей"))
-    elif type != '1':
-        return render_template('index.html', ans='Выберите валюту!')
+        return render_template('index.html', ans=str(str(sum) + " Рублей - " + str(round(answer,2)) + " Евро"))
+    if type == '3':  # Доллар
+        answer = sum * 80.06
+        return render_template('index.html', ans=str(str(sum) + " Доллар - " + str(round(answer,2)) + " Рублей"))
+    if type == '4':  # Евро
+        answer = sum * 85.9
+        return render_template('index.html', ans=str(str(sum) + " Евро - " + str(round(answer,2)) + " Рублей"))
+
 
 if __name__ == '__main__':
     app.run()
